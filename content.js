d4bf5e4a7c26df3347abd1c93fe09b0c006d1b2f
FrancescoDiCursi <79453817+FrancescoDiCursi@users.noreply.github.com>
2023-10-18 19:30:49 +0200

    Update content.js

--- content.js
+++ content.js
@@ -1,7 +1,6 @@
 window.addEventListener('load',()=>{
     //alert("LOADED")
     //re apply promises on each click
-
     window.addEventListener('click',()=>{
         console.log("Promises reapplied")
         //skip the skippable
@@ -10,18 +9,39 @@ window.addEventListener('load',()=>{
             console.log("Skippable skipped")
         });
         //hide the unskippable
-        waitForElm('ytp-ad-player-overlay-instream-info').then((popup)=>{
+        waitForElm('.ytp-ad-player-overlay-instream-info').then((popup)=>{
 
 
             var elm= document.querySelector(".video-stream.html5-main-video")
             elm.src=""
             console.log("Unskippable skipped")
-            document.body.click()
-            document.body.click()
+
 
 
         })
     })
+
+    
+    
+    //skip the skippable
+    waitForElm('.ytp-ad-skip-button.ytp-button').then((elm) => {
+        elm.click()
+        console.log("Skippable skipped")
+    });
+    //hide the unskippable
+    waitForElm('.ytp-ad-player-overlay-instream-info').then((popup)=>{
+
+
+        var elm= document.querySelector(".video-stream.html5-main-video")
+        elm.src=""
+        console.log("Unskippable skipped")
+
+
+
+    })
+
+
+
 
     document.body.click()
     document.body.click()
@@ -49,3 +69,32 @@ function waitForElm(selector) {
         });
     });
 }
+
+function reloadPage() {
+
+    // The last "domLoading" Time //
+    
+    var currentDocumentTimestamp =
+    
+    new Date(performance.timing.domLoading).getTime();
+    
+    // Current Time //
+    
+    var now = Date.now();
+    
+    // Ten Seconds //
+    
+    var tenSec = 10 * 1000;
+    
+    // Plus Ten Seconds //
+    
+    var plusTenSec = currentDocumentTimestamp + tenSec;
+    
+    if (now > plusTenSec) {
+    
+    location.reload();
+    
+    } else {}
+    
+    }
+    
