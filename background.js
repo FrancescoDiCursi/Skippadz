dc74f8fd995a80d1fce78763f734c1a8a9394e44
FrancescoDiCursi <79453817+FrancescoDiCursi@users.noreply.github.com>
2023-10-24 12:10:06 +0200

    removing files

--- background.js
+++
@@ -1,7 +0,0 @@
-//init the storage
-chrome.storage.local.get((strg)=>{
-    console.log(strg)
-    if(!Object.keys(strg).includes("auto_quality_check") || !Object.keys(strg).includes("time_tracker_els") ||  !Object.keys(strg).includes("time_tracker")){
-        chrome.storage.local.set({"auto_quality_check":"off", "time_tracker_els":[0,0,""], "time_tracker":'off'})
-    }
-})
